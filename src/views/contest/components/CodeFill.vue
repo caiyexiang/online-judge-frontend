5d13529d9811c4a9a38a41013373ff4ff1336cc7
caiyexiang <caiyexiang1@live.com>
2020-05-15 15:17:31 +0800

    feat: 代码补全题支持轮询结果

--- src/views/contest/components/CodeFill.vue
+++ src/views/contest/components/CodeFill.vue
@@ -1,8 +1,14 @@
 <script>
+import openWindow from '@/utils/open-window'
+import { mapGetters } from 'vuex'
+import { getCodeFillSubmissions } from '@/api/contest'
 import CodeMirror from '@/components/CodeMirror'
 import { createCodeFillSubmission } from '@/api/contest'
 import { fillRegEx } from '@/utils/constant'
 export default {
+  components: {
+    CodeMirror,
+  },
   props: {
     problem: {
       type: Object,
@@ -17,11 +23,24 @@ export default {
     return {
       answer: [...this.problem.answer],
       contest: parseInt(this.$route.params.id),
+      result: null,
+      msg: '',
+      loading: false,
     }
   },
-  components: {
-    CodeMirror,
+  computed: {
+    ...mapGetters(['username']),
+    msgType() {
+      if (this.msg === '正在运行...') {
+        return 'info'
+      } else if (this.msg === 'Accepted') {
+        return 'success'
+      } else {
+        return 'danger'
+      }
+    },
   },
+
   methods: {
     createCodeFillSubmission() {
       if (this.answer.length === 0) {
@@ -37,6 +56,7 @@ export default {
       createCodeFillSubmission(data)
         .then(_ => {
           this.$message.success('提交成功')
+          this.pollSubmission()
         })
         .catch(e => {
           this.$message.error('提交失败')
@@ -45,6 +65,48 @@ export default {
     },
     updateAnswer() {
       this.$emit('updateAnswer', this.answer)
+    },
+    pollSubmission() {
+      this.result = null
+      this.msg = '正在运行...'
+      this.loading = true
+      let counter = 0
+      const timer = setInterval(() => {
+        counter++
+        if (counter > 5) {
+          clearInterval(timer)
+          return
+        }
+        const params = {
+          offset: 0,
+          limit: 1,
+          contest: this.contest,
+          problem: this.problem.id,
+          search: this.username,
+        }
+        getCodeFillSubmissions(params)
+          .then(({ results }) => {
+            const result = results[0]
+            if (result.status !== 'waiting') {
+              this.loading = false
+              this.msg = result.status
+              this.result = result
+              this.dialogVisible = true
+              clearInterval(timer)
+            }
+          })
+          .catch(_ => {
+            this.msg = '网络错误'
+            this.loading = false
+            clearInterval(timer)
+          })
+      }, 1000)
+      this.$once('hook:beforeDestroy', () => {
+        timer && clearInterval(timer)
+      })
+    },
+    openSubmission() {
+      openWindow(`/code-fill-submission/${this.result.id}/`)
     },
   },
   render() {
@@ -58,6 +120,20 @@ export default {
       ) : (
         undefined
       )
+    const resultButton = this.result ? (
+      <el-button type="primary" vOn:click={this.openSubmission}>
+        查看结果
+      </el-button>
+    ) : (
+      undefined
+    )
+    const statusButton = this.msg ? (
+      <el-button type={this.msgType} plain>
+        {this.msg}
+      </el-button>
+    ) : (
+      undefined
+    )
     for (let i = 0; i < codeTemplateList.length; i++) {
       codeElement.push(
         <CodeMirror
@@ -98,10 +174,12 @@ export default {
           style="margin-top:20px"
           disabled={this.finished}
           type="primary"
-          vOn:click_native={this.createCodeFillSubmission}
+          vOn:click={this.createCodeFillSubmission}
         >
           提交代码
         </el-button>
+        {statusButton}
+        {resultButton}
       </div>
     )
   },
